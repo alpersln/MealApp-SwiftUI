cfa66cd51eb74de3ab02bcd87b746c676bd3143d
replappdev <replappdev@gmail.com>
2022-09-18 19:45:20 +0300

    Initial Commit

---
+++ MealApp-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealApp-SwiftUI
+//
+//  Created by Alper Sülün on 18.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealApp-SwiftUI/MealApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealApp_SwiftUIApp.swift
+//  MealApp-SwiftUI
+//
+//  Created by Alper Sülün on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MealApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
